68c1e05c52c1ec5225d4ab2bbf73259c93d7f887
James Boocock <james.boocock@otago.ac.nz>
2015-06-07 18:08:56 +1200

    Added paintor installation file

--- get_submodules.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-#
-git submodule init
-git submodule update
-git pull origin master
-git submodule update --recursive
---
+++ install_paintor.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# Make PAINTOR.
+
+cd PAINTOR_FineMapping/
+bash install.sh
+sudo cp PAINTOR /usr/bin
